d60eaad91bb4cbe515514a6d9bfe0a96223ab75b
Mateus Marques <55675189+Mateusds@users.noreply.github.com>
2026-03-22 16:09:38 -0300

    Ajustando config para dominio próprio na EC2

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repository = process.env.GITHUB_REPOSITORY || "";
-const repoName = repository.split("/")[1] || "";
-const isUserOrOrgPages = repoName.endsWith(".github.io");
-const basePath =
-  isGithubActions && repoName && !isUserOrOrgPages ? `/${repoName}` : "";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
+  output: "export",      // Essencial para o deploy na EC2
+  trailingSlash: true,   // Ajuda o Nginx a achar as pastas
   images: {
-    unoptimized: true
+    unoptimized: true    // Necessário para o 'next export'
   },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined
+  // Remova o basePath e assetPrefix para usar o domínio direto
 };
 
 export default nextConfig;
